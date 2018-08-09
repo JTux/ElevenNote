7bcb10c5d5ded41734766d89f839d0a246469b60
Joshua Tucker <thejltucker@gmail.com>
2018-08-09 13:34:58 -0400

    Functioning ElevenNote

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        public Note()
+        {
+        }
+
+        public Note(string title, string content, Guid ownerId, DateTimeOffset createdTime)
+        {
+            Title = title;
+            Content = content;
+            OwnerID = ownerId;
+            CreatedUtc = createdTime;
+        }
+
+        [Key]
+        public int NoteID { get; set; }
+
+        [Required]
+        public Guid OwnerID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+        [DefaultValue(false)]
+        public bool IsStarred { get; set; }
+    }
+}
